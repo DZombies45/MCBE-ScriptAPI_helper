2161084c48166dd3aa80a8929caaa9a51c1ede81
jombie <dewan.dinda@gmail.com>
2026-01-21 19:41:53 +0700

    add signal system

---
+++ ts/dz-signal.ts
@@ -0,0 +1,110 @@
+import { ScriptEventSource, system } from "@minecraft/server";
+
+// ----- types
+
+type requestData = {
+  id: string; // id yang ingin di call
+  requesterId: string; // id sender
+  value: any; // isi requesan
+};
+
+type callbackF = (data: requestData) => void;
+
+// ----- default
+
+const callbackDefault = (data: requestData) => {
+  console.warn(
+    `signal rechived id: ${data.id},${data.requesterId === "" ? "" : ` and sender: ${data.requesterId}`} that contains: ${data.value}`,
+  );
+};
+
+// ----- signal maneger
+
+export class SignalManager {
+  private signalPending: Map<string, callbackF> = new Map();
+  private signalConnented: Map<string, Set<callbackF>> = new Map();
+  private namespace: string = "dz";
+  private signalId: string = "";
+  private static readonly Timeout: number = 100;
+  constructor(namespace: string = "dz", signal: string = "signal") {
+    this.namespace = namespace;
+    this.signalId = `${namespace}:${signal}`;
+    this.start();
+  }
+  public emitAndListent(id: string, value: any): Promise<requestData> {
+    return new Promise((resolve, reject) => {
+      const requestId: string = Math.random().toString(36).substring(2, 12);
+
+      const timeout = system.runTimeout(() => {
+        this.signalPending.delete(requestId);
+        reject(new Error(`Signal timeout: ${id}`));
+      }, SignalManager.Timeout);
+
+      this.signalPending.set(requestId, (data) => {
+        system.clearRun(timeout);
+        resolve(data);
+      });
+
+      this.emit(id, value, requestId);
+    });
+  }
+  public emit(id: string, value: any, requesterId: string = ""): void {
+    const data: requestData = {
+      id,
+      requesterId,
+      value,
+    };
+    system.sendScriptEvent(this.signalId, JSON.stringify(data));
+  }
+  public connect(id: string, callback: callbackF = callbackDefault): boolean {
+    let listeners = this.signalConnented.get(id);
+    if (!listeners) {
+      listeners = new Set();
+      this.signalConnented.set(id, listeners);
+    }
+    listeners.add(callback);
+    return true;
+  }
+  public disconnect(id: string): boolean {
+    if (!this.signalConnented.has(id)) return false;
+    this.signalConnented.delete(id);
+    return true;
+  }
+  private start(): void {
+    system.afterEvents.scriptEventReceive.subscribe(
+      ({ id, message, sourceType }) => {
+        try {
+          if (id !== this.signalId || sourceType !== ScriptEventSource.Server)
+            return;
+
+          const data: requestData = JSON.parse(message);
+
+          const listeners = this.signalConnented.get(data.id);
+          if (listeners)
+            for (const listener of listeners) {
+              try {
+                listener(data);
+              } catch (e) {
+                console.error(`[Signal:${data.id}] listener error`, e);
+              }
+            }
+
+          if (this.signalPending.has(data.id)) {
+            const pending = this.signalPending.get(data.id);
+            try {
+              pending(data);
+              this.signalPending.delete(data.id);
+            } catch (e) {
+              console.error(`[Signal:${data.id}] pending error`, e);
+            }
+          }
+        } catch (e) {
+          console.error(`[${this.signalId}] error: ${e}`);
+        }
+      },
+      { namespaces: [this.namespace] },
+    );
+  }
+}
+
+export const SIGNAL = new SignalManager("dz", "signal");
